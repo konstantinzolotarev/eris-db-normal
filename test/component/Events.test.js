b46696fd6a2fd4f7cc83264eb181526ce64f682f
Konstantin Zolotarev <konstantin.zolotarev@pixster.eu>
2017-01-13 16:16:06 +0200

    events + test

--- test/component/Events.test.js
+++ test/component/Events.test.js
@@ -12,4 +12,90 @@ describe('Events :: ', () => {
 
   })
 
+  describe('eventSubscribe() :: ', () => {
+
+    it('reject without eventId', () => {
+      return global.erisdb
+        .events
+        .eventSubscribe()
+        .then(() => Promise.reject())
+        .catch((err) => {
+          expect(err).to.be.an('error')
+            .and.to.have.property('message', 'EventId is required parameter')
+        })
+    })
+
+    it('should subscribe to event', () => {
+      const eventId = 'NewBlock'
+      return global.erisdb
+        .events
+        .eventSubscribe(eventId)
+        .then((info) => {
+          expect(info).to.be.an('object')
+            .and.to.have.property('sub_id')
+        })
+    })
+  })
+
+  describe('eventPoll() :: ', () => {
+
+    it('reject without eventId', () => {
+      return global.erisdb
+        .events
+        .eventPoll()
+        .then(() => Promise.reject())
+        .catch((err) => {
+          expect(err).to.be.an('error')
+            .and.to.have.property('message', 'EventId is required parameter')
+        })
+    })
+
+    it('should not poll to not active event', () => {
+      const eventId = 'NewBlock'
+      return global.erisdb
+        .events
+        .eventPoll(eventId)
+        .catch((err) => {
+          expect(err).to.be.an('object')
+            .and.to.have.property('message')
+        })
+    })
+
+    xit('should poll to event', () => {
+      const eventId = 'NewBlock'
+      return global.erisdb
+        .events
+        .eventPoll(eventId)
+        .then((info) => {
+          expect(info).to.be.an('object')
+            .and.to.have.property('events')
+        })
+    })
+  })
+
+  describe('eventUnsubscribe() :: ', () => {
+
+    it('reject without eventId', () => {
+      return global.erisdb
+        .events
+        .eventUnsubscribe()
+        .then(() => Promise.reject())
+        .catch((err) => {
+          expect(err).to.be.an('error')
+            .and.to.have.property('message', 'EventId is required parameter')
+        })
+    })
+
+    it('should unsubscribe to event', () => {
+      const eventId = 'NewBlock'
+      return global.erisdb
+        .events
+        .eventUnsubscribe(eventId)
+        .then((info) => {
+          expect(info).to.be.an('object')
+            .and.to.have.property('result')
+        })
+    })
+  })
+
 })
